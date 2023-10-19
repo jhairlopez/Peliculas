d625665e6f0d591201b5bac55f344f6a414f3d76
jhairlopez <riosjhair11@gmail.com>
2023-10-19 00:14:22 -0500

    Se muestra las imagenes y los titulos de la Api consumida

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
+let pagina = 1;
+const anterior = document.getElementById("btnAnterior")
+const siguiente = document.getElementById("btnSiguiente")
+
+siguiente.addEventListener('click', () =>{
+    if(pagina < 1000){
+        pagina += 1;
+        cargarPeliculas();
+    }
+})
+
+anterior.addEventListener('click', () =>{
+    if(pagina > 1){
+        pagina -= 1;
+        cargarPeliculas();
+    }
+})
+
 const  cargarPeliculas = async () =>{
-    let respuesta = await fetch('https://api.themoviedb.org/3/movie/550?api_key=55e0fba2d3a2e7b5442298bf69fc0ba5');
-    console.log(respuesta);
+    try{
+        let respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=55e0fba2d3a2e7b5442298bf69fc0ba5&language=es-MX&page=${pagina}`);
+        if(respuesta.status == 200){
+            let datos = await  respuesta.json();
+            let peliculas = ''
+
+            datos.results.forEach(peli => {
+                peliculas +=`
+                <div class="pelicula">
+                    <img src="https://image.tmdb.org/t/p/w500/${peli.poster_path}">                
+                    <h3 class="title">${peli.title}</h3>
+                </div>
+                 `;
+            });
+
+            document.getElementById('contenedor').innerHTML = peliculas
+        // console.log(datos)
+
+        }else if(respuesta.status === 401){
+            console.log("Pusiste mal la llave")
+        }else if (respuesta.status === 404){
+            console.log("La Película no Existe")
+        }else{
+            console.log("Hubo un Error en el Sistema")
+        }
+       
+    }
+    catch(error){
+        console.error("No se encontro la Api")
+    }
+    // console.log(respuesta);
 } 
 cargarPeliculas()
 
